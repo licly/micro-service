276f2e4756f59b562fd5b9fadddf1f4ea65f61ab
licly <liclyn@163.com>
2020-10-07 23:49:13 +0800

    完善网关代码

---
+++ spring-cloud/gateway/src/main/java/com/licly/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.licly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * gateway网关
+ *
+ * @author licly
+ * @date 2020/10/7
+ */
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
